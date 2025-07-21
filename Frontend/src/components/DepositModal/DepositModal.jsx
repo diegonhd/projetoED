5c1046abc900f6573283a29d10f51e6ea34ee472
Antônio Sousa <antoniocsousa89@gmail.com>
2025-07-21 13:30:13 -0300

    iniciei a janela modal de depósito

--- Frontend/src/components/DepositModal/DepositModal.jsx
+++ Frontend/src/components/DepositModal/DepositModal.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Modal from "../Modal/Modal.jsx";
+import "./DepositModal.css"
 
 export default function DepositModal(props) {
     return (
         <Modal isOpen={props.isOpen} setStade={props.setStade}>
-            <p>Depósito</p>
+            <form className="deposit-container">
+                <label>Valor a depositar</label>
+                <input type="text" required placeholder="R$ 0.00"/>
+                <button>Depositar</button>
+            </form>
         </Modal>
     )
 }
